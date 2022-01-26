936656bd27b75fd7e220dca57b28092c7803c0ac
tboenig <mschulz@bbaw.de>
2022-01-26 21:19:49 +0100

    update folder structure

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-metsfiles=`ls data_structure/raw/`
+metsfiles=`ls data_structure/`
 for eachfile in $metsfiles
 do
-   # echo cd ../data_structure/raw/$eachfile; ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data_structure/raw/$eachfile/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
-   cd /home/runner/work/example-gt-repo/example-gt-repo/data_structure/raw/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data_structure/raw/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
+   cd /home/runner/work/example-gt-repo/example-gt-repo/data_structure/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data_structure/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
    
    
    
