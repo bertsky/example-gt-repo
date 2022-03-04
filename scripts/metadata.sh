a68f99b0b033c478e59a4c952585e7b19e2f3ccc
tboenig <mschulz@bbaw.de>
2022-03-04 19:26:11 +0100

    Update metadata.sh

--- scripts/metadata.sh
+++ scripts/metadata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repN=`${{ github.repository.name}}`
+repN = ${{ github.repository.name}} 
 
 
    sudo java -jar saxon-he-10.5.jar -xsl:scripts/gt-overview_metadata.xsl output=METADATA repoName=$repN -s:scripts/gt-overview_metadata.xsl -o:ghout/metadata.md
