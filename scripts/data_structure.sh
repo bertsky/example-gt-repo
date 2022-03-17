faada8abc33d98707b1e50ca9b50978abd5915e1
Matthias Boenig <mschulz@bbaw.de>
2022-03-17 19:00:45 +0100

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-metsfiles=`ls data_structure/`
+metsfiles=`ls data/`
 for eachfile in $metsfiles
 do
-   cd /home/runner/work/example-gt-repo/example-gt-repo/data_structure/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data_structure/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
+   cd /home/runner/work/example-gt-repo/example-gt-repo/data/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
    
    
    
