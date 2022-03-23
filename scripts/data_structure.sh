0f0d186b854cbbd410e6026c7d5545fe36404d76
tboenig <mschulz@bbaw.de>
2022-03-23 14:05:03 +0100

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-path=`pwd`
 metsfiles=`ls data/`
+path=`pwd`
+
 for eachfile in $metsfiles
 do
-   cd $path/data/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv $path/data/*.zip $path/ocrdzip_out/
+cd $path/data/$eachfile
+
+if test -f "mets.xml"; then
+    cd $path/data/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv $path/data/*.zip $path/ocrdzip_out/
+
+else
+ 
+    echo "sh mets.sh"
+
+fi
+
+
+
+   
    
    
    
 done
+
+
+
+
+
