65b0691d65fa00de97049a2c4a5d3ee7c640926c
tboenig <mschulz@bbaw.de>
2022-03-23 13:18:26 +0100

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-path="$(find -name "mets.xml" -execdir pwd \; -quit)"
+path=`pwd`
 metsfiles=`ls data/`
 for eachfile in $metsfiles
 do
-   
-   echo "cd /home/runner/work/example-gt-repo/example-gt-repo/data/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/"
+   cd $path/data/$eachfile;ocrd zip bag -i ocrd_data_structur_$eachfile;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
    
    
    
