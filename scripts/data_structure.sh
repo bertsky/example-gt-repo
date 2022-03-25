b231d0fe38fb947f0f77bd26df7645bbb0661477
tboenig <mschulz@bbaw.de>
2022-03-25 01:21:41 +0100

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -3,18 +3,18 @@ metsfiles=`ls data/`
 page=`find -name 'GT-PAGE'`
 path=`pwd`
 
-cd /home/runner/work/DTGT/DTGT/data/Die_paepstliche_Unfehlbarkeit/;pwd
+
 for eachfile in $page
 do
 cd $path/$eachfile;cd ..
 p=`pwd`
 if test -f "mets.xml"; then
-    cd $path/$eachfile;cd ..;ocrd zip bag -i ocrd_data_structur_${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
+    cd $p;cd ..;ocrd zip bag -i ocrd_data_structur_${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
     
 
 else
  
-    cd $path/$eachfile;cd ..;ocrd workspace init ${PWD##*/};sudo mkdir $path/ghout/${PWD##*/};sudo mv $p/mets.xml $path/ghout/${PWD##*/}/
+    cd $p;ocrd workspace init ${PWD##*/};sudo mkdir $path/ghout/${PWD##*/};sudo mv $p/mets.xml $path/ghout/${PWD##*/}/
     
 fi
 done
