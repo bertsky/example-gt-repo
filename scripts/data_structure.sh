7572d657428c4bc86db022443ef76c16098b33a0
tboenig <mschulz@bbaw.de>
2022-03-29 19:27:02 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -7,7 +7,7 @@ do
 cd $path/$eachfile;cd ..
 p=`pwd`
    echo $eachfile
-   cd $p;ocrd zip bag -i ocrd_data_structur_$eachfile -I DEST;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
+   cd data/$p;ocrd zip bag -i ocrd_data_structur_$eachfile -I DEST;sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
    
    
    
