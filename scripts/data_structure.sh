8b12a01a8052dcf76f204ca5943503da44c05e43
tboenig <mschulz@bbaw.de>
2022-03-29 15:49:13 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -9,7 +9,11 @@ do
 cd $path/$eachfile;cd ..
 p=`pwd`
 if test -f "mets.xml"; then
-    cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/} -I .;cd ..;pwd;ls -l
+
+    cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
+    
+
+    # cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/} -I .;cd ..;pwd;ls -l
     
     # ocrd zip bag -i ocrd_data_structur_${PWD##*/};pwd;ls -l;cd ${PWD##*/};pwd;ls -l;
     # sudo mv $path/data/*.zip $path/ocrdzip_out/
