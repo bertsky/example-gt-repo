cf6d8746b2a21d5f125fc01637e35efa10c2747c
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-03-30 01:29:13 +0200

    data_structure.sh: sanitize

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-metsfiles=`ls data/`
-page=`find -name 'GT-PAGE'`
-path=`pwd`
 
-
-for eachfile in $page
+topdir="$PWD"
+for workspace in $(find -name GT-PAGE -type d)
 do
-cd $path/$eachfile;cd ..
-p=`pwd`
-if test -f "mets.xml"; then
-    cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
-    
-
-else
-    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;sudo mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
-fi
+    # chdir to workspace
+    pushd $(dirname "$workspace")
+    name=$(basename "$PWD")
+    if test -f "mets.xml"; then
+        ocrd zip bag -i ocrd_data_structur_$name
+        sudo mv $workspace.ocrd.zip "$topdir"/ocrdzip_out/
+    else
+        ocrd workspace init
+        sh "$topdir"/ghout/mets.sh
+        sudo mkdir "$topdir"/ghout/$name
+        sudo mv mets.xml $path/ghout/$name/
+    fi
 done
 
 
