07affc378213d0a3beeeb6646b3b9a3f0e4dd67d
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-03-30 01:57:22 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -4,16 +4,19 @@ topdir="$PWD"
 for workspace in $(find -name GT-PAGE -type d)
 do
     # chdir to workspace
-    pushd $(dirname "$workspace")
+    pushd "$workspace"/..
     name=$(basename "$PWD")
+    if $name = data; then
+        name=$(basename $(dirname "$PWD"))
+    fi
     if test -f "mets.xml"; then
         ocrd zip bag -i ocrd_data_structur_$name
-        sudo mv $workspace.ocrd.zip "$topdir"/ocrdzip_out/
+        sudo mv $PWD.ocrd.zip "$topdir"/ocrdzip_out/
     else
         ocrd workspace init
         sh "$topdir"/ghout/mets.sh
         sudo mkdir "$topdir"/ghout/$name
-        sudo mv mets.xml $path/ghout/$name/
+        sudo mv mets.xml "$topdir"/ghout/$name/
     fi
 done
 
