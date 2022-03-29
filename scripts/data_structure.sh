79e3814b5c56444f9c10480e6aa3ec61493d9709
tboenig <mschulz@bbaw.de>
2022-03-29 20:08:32 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -17,29 +17,4 @@ else
 fi
 done
 
-# ------- alt
-
-#!/bin/bash
-#metsfiles=`ls data/`
-#page=`find -name 'GT-PAGE'`
-#path=`pwd`
-
-
-#for eachfile in $page
-#do
-#cd $path/$eachfile;cd ..
-#p=`pwd`
-#if test -f "mets.xml"; then
-    
- #   cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/} -I -Z zip_${PWD##*/}
-    # ocrd zip bag -i ocrd_data_structur_${PWD##*/} -I test;
-    # find -name ocrd_data_structur_${PWD##*/};sudo mv /home/runner/work/example-gt-repo/example-gt-repo/data/*.zip /home/runner/work/example-gt-repo/example-gt-repo/ocrdzip_out/
-    # cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
-    
-#else
-    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};pwd;ls -l
-    # cd ${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
-    # sudo mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
-#fi
-#done
 
