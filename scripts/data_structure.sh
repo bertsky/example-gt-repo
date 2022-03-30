c6c24ee124d820dccc58a384c480bc7ec4548cb5
Matthias Boenig <mschulz@bbaw.de>
2022-03-30 12:37:46 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -9,11 +9,13 @@ do
 cd $path/$eachfile;cd ..
 p=`pwd`
 if test -f "mets.xml"; then
-    cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};sudo mv $path/data/*.zip $path/ocrdzip_out/
+    cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};mv $path/data/*.zip $path/ocrdzip_out/
     
 
 else
-    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;sudo mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
+    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};mv $path/data/*.zip $path/ocrdzip_out/
+    
+    # ;mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
 fi
 done
 
