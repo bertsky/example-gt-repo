f9bc5b1d9a54879c41790cba5aaf6df65cf0fed5
tboenig <mschulz@bbaw.de>
2022-03-30 14:54:45 +0200

    Update data_structure.sh

--- scripts/data_structure.sh
+++ scripts/data_structure.sh
@@ -13,11 +13,13 @@ if test -f "mets.xml"; then
     
 
 else
-    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};cd ${PWD##*/};pwd;ls -l
+    ocrd workspace --directory $p init;sh $path/ghout/mets.sh;cd $p;ocrd zip bag -i ocrd_data_structur_${PWD##*/};mv $path/data/*.zip $path/ocrdzip_out/
+    
+    # pwd;ls -l
     
     # mv $path/data/*.zip $path/ocrdzip_out/
     
-    # ;mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
+    # mkdir $path/ghout/${PWD##*/};sudo mv mets.xml $path/ghout/${PWD##*/}/
 fi
 done
 
